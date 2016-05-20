f8e8cbfbe4e8e0251d0b2bca880e81f545911155
jcgarsan <jc.garsan@gmail.com>
2016-05-20 12:50:52 +0200

    Package structure done. userControlAlarm and safetyMeasuresAlarm callbacks done

--- include/hrov_arm_control/hrov_arm_control.h
+++ include/hrov_arm_control/hrov_arm_control.h
@@ -15,6 +15,14 @@
 
 //ROS
 #include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Int8MultiArray.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+#include <mar_robot_arm5e/ARM5Arm.h>
 
 
 using namespace std;
@@ -26,6 +34,31 @@ class Hrov_arm_control
 	public:
 		Hrov_arm_control();
 		~Hrov_arm_control();
+		
+		bool	robotControl;
+		
+		std_msgs::Int8MultiArray	safetyMeasureAlarm;
+		std_msgs::Int8MultiArray	userControlAlarm;
 
+		ARM5Arm *robot;
+		vpHomogeneousMatrix desired_bMe, bMe;
+		vpColVector next_joints;
+
+
+	private:
+		void safetyMeasuresCallback(const std_msgs::Int8MultiArray::ConstPtr& msg);
+		void userControlCallback(const std_msgs::Int8MultiArray::ConstPtr& msg);
+
+
+		ros::NodeHandle nh;
+
+		ros::Subscriber sub_safetyMeasures;
+		ros::Subscriber sub_userControl;
+
+		tf::Transform 				transform_init, transform_new;
+		tf::StampedTransform 		transform;
+		tf::Quaternion				q_init, q_new;
+		tf::TransformBroadcaster	br;
+		tf::TransformListener		*listener;
 
 };
--- src/hrov_arm_control.cpp
+++ src/hrov_arm_control.cpp
@@ -14,24 +14,70 @@
 #include <string.h>
 #include "../include/hrov_arm_control/hrov_arm_control.h"
 
-/*#include <visp/vpColVector.h>
+#include <visp/vpColVector.h>
 #include <visp/vpHomogeneousMatrix.h>
-#include <cmath>  */
+#include <cmath>
+
+#define num_sensors			2		// 0 = is there an alarm?, 1 = surface, 2 = seafloor
+
+//DEBUG Flags
+#define DEBUG_FLAG_SAFETY	0
+#define DEBUG_FLAG_USER		0
 
 
 using namespace std;
 
 
-
 Hrov_arm_control::Hrov_arm_control()
 {
 	//initializing values
+	robotControl = true;
+
+	for (int i=0; i<=num_sensors; i++)
+		safetyMeasureAlarm.data.push_back(0);
+	for (int i=0; i<2; i++)
+		userControlAlarm.data.push_back(0);
+
+	//Subscriber initialization (sensors)
+	sub_safetyMeasures = nh.subscribe<std_msgs::Int8MultiArray>("safetyMeasuresAlarm", 1, &Hrov_arm_control::safetyMeasuresCallback, this);
+	sub_userControl = nh.subscribe<std_msgs::Int8MultiArray>("userControlAlarm", 1, &Hrov_arm_control::userControlCallback, this);
+
+	//Arm control
+	robot = new ARM5Arm(nh, "uwsim/joint_state", "uwsim/joint_state_command");
+
+
 
 }
+
 
 Hrov_arm_control::~Hrov_arm_control()
 {
 	//Destructor
+}
+
+
+void Hrov_arm_control::safetyMeasuresCallback(const std_msgs::Int8MultiArray::ConstPtr& msg)
+{
+	for (int i=0; i<=num_sensors; i++)
+		safetyMeasureAlarm.data[i] = msg->data[i];
+
+	if (DEBUG_FLAG_SAFETY)
+	{
+		cout << "safetyMeasureAlarm: [";
+		for (int i=0; i<=num_sensors; i++)
+			cout << (int) safetyMeasureAlarm.data[i] << ",";
+		cout << "]" << endl;
+	}
+}
+
+
+void Hrov_arm_control::userControlCallback(const std_msgs::Int8MultiArray::ConstPtr& msg)
+{
+	for (int i=0; i<2; i++)
+		userControlAlarm.data[i] = msg->data[i];
+
+	if (DEBUG_FLAG_USER)
+		cout << "userControlCallback: [" << (int) userControlAlarm.data[0] << ", " << (int) userControlAlarm.data[1] << "]" << endl;
 }
 
 
